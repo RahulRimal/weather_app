c5f89c078ce7c6323f0f267057a2605320653236
Rahul Rimal <genuinetechnology9@gmail.com>
2020-08-07 07:49:48 +0545

    This is a complete weather app

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyWeatherApp());
+
+class MyWeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
